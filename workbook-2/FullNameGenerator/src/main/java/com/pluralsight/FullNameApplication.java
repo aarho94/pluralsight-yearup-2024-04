7c2b30e7d6a447838042a116d9f23562e92c133d
Aaron J. Rhodes <aaronj.rhodes@aarons-mbp.mynetworksettings.com>
2024-04-15 13:52:45 -0700

    FullNameGenerator

---
+++ workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+import java.util.Scanner;
+public class FullNameApplication
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        // prompt user to enter name
+        System.out.println("Enter your first name: ");
+        String firstName = scanner.nextLine().trim();
+
+        System.out.println("Enter your middle name (if you do not have a middle name, leave blank): ");
+        String middleName = scanner.nextLine().trim();
+
+        System.out.println("Enter your last name: ");
+        String lastName = scanner.nextLine().trim();
+
+        System.out.print("Enter suffix (if applicable): ");
+        String suffix = scanner.nextLine().trim();
+
+        // build the name
+        StringBuilder nameComponents = new StringBuilder();
+        nameComponents.append(firstName);
+        nameComponents.append(" ");
+        if (!middleName.isEmpty()) {
+            nameComponents.append(middleName);
+            nameComponents.append(" ");
+        }
+        nameComponents.append(lastName);
+        if (!suffix.isEmpty()) {
+            nameComponents.append(", ");
+            nameComponents.append(suffix);
+        }
+
+        String fullName = nameComponents.toString();
+
+        // print the name
+        System.out.println("Full name: " + fullName);
+
+        scanner.close();
+    }
+
+}
